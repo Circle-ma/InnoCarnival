3d245b59a072b6b537151a7134448b2324635c0a
Circle-ma <a859576084@gmail.com>
2022-08-04 15:14:05 +0800

    real sample

--- src/Questions.ts
+++ src/Questions.ts
@@ -9,57 +9,57 @@ export type QuestionsProps = {
 const Questions: QuestionsProps[] = [
   {
     questionNumber: 1,
-    topic: "1. What is Your Name?",
+    topic: "第一題",
     description:
-      "Finding the red rose in the mailbox was a pleasant surprise for Sarah. She didn't have a boyfriend or know of anyone who was interested in her as anything more than a friend. There wasn't even a note attached to it. Although it was a complete mystery, it still made her heart jump and race a little more than usual. She wished that she could simply accept the gesture and be content knowing someone had given it to her, but that wasn't the way Sarah did things. Now it was time to do a little detective work and try to figure who had actually left the red rose.",
-    option: ["A: dragon", "B: creeper", "C: crab", "D: happy"],
-    Answer: "A",
-  },
-  {
-    questionNumber: 2,
-    topic: "2. How old are you?",
-    description:
-      "There was no ring on his finger. That was a good sign although far from proof that he was available. Still, it was much better than if he had been wearing a wedding ring on his hand. She glanced at his hand a bit more intently to see if there were any tan lines where a ring may have been, and he's simply taken it off. She couldn't detect any which was also a good sign and a relief. The next step would be to get access to his wallet to see if there were any family photos in it.",
-    option: ["A", "B", "C", "D"],
-    Answer: "B",
-  },
-  {
-    questionNumber: 3,
-    topic: "3. Where are you from?",
-    description:
-      "She glanced up into the sky to watch the clouds taking shape. First, she saw a dog. Next, it was an elephant. Finally, she saw a giant umbrella and at that moment the rain began to pour.",
-    option: ["A", "B", "C", "D"],
-    Answer: "C",
-  },
-  {
-    questionNumber: 4,
-    topic: "4. Why you are here?",
-    description:
-      "Sometimes it's just better not to be seen. That's how Harry had always lived his life. He prided himself as being the fly on the wall and the fae that blended into the crowd. That's why he was so shocked that she noticed him.",
-    option: ["A", "B", "C", "D"],
+      "本項目透過什麽來形成帕金森病步態綜合量化評價指標？",
+    option: ["A:	肌電信號", "B:	運動學參數", "C:	動力學參數", "D:	以上皆是"],
     Answer: "D",
   },
   {
-    questionNumber: 5,
-    topic: "5.What do you want to be?",
+    questionNumber: 2,
+    topic: "第二題",
     description:
-      "She reached her goal, exhausted. Even more chilling to her was that the euphoria that she thought she'd feel upon reaching it wasn't there. Something wasn't right. Was this the only feeling she'd have for over five years of hard work?",
-    option: ["A", "B", "C", "D"],
-    Answer: "B",
+      "可穿戴健康監測與帕金森智能診斷系統能夠為帕金遜病人提供什麽？",
+    option: ["A:	治愈帕金森病人", "B:	幫助帕金森病人穩定動作", "C:	便捷且準確的日常監測與早期診斷服務", "D:	防止帕金森神經退行性疾病"],
+    Answer: "C",
   },
   {
-    questionNumber: 6,
-    topic: "6.What do you want to be?",
+    questionNumber: 3,
+    topic: "第三題",
     description:
-      "She reached her goal, exhausted. Even more chilling to her was that the euphoria that she thought she'd feel upon reaching it wasn't there. Something wasn't right. Was this the only feeling she'd have for over five years of hard work?",
-    option: ["A", "B", "C", "D"],
-    Answer: "B",
+      "以下哪一項不是可穿戴健康監測與帕金森智能診斷系統的優勢？",
+    option: ["A:	針對性強", "B:	前瞻性強", "C:	輕便可靠", "D:	保護性強"],
+    Answer: "D",
   },
+  {
+    questionNumber: 4,
+    topic: "第四題",
+    description:
+      "如何應用可穿戴健康監測與帕金森智能診斷系統？",
+    option: ["A:	老年人的健康狀態評估", "B:	帕金森病早期診斷和治療的精準健康護理", "C:	對帕金森患者的即時長期監測與突發事件報警", "D:	以上皆是"],
+    Answer: "D",
+  },
+  // {
+  //   questionNumber: 5,
+  //   topic: "5.What do you want to be?",
+  //   description:
+  //     "She reached her goal, exhausteD: Even more chilling to her was that the euphoria that she thought she'd feel upon reaching it wasn't there. Something wasn't right. Was this the only feeling she'd have for over five years of hard work?",
+  //   option: ["A", "B", "C", "D"],
+  //   Answer: "B",
+  // },
+  // {
+  //   questionNumber: 6,
+  //   topic: "6.What do you want to be?",
+  //   description:
+  //     "She reached her goal, exhausteD: Even more chilling to her was that the euphoria that she thought she'd feel upon reaching it wasn't there. Something wasn't right. Was this the only feeling she'd have for over five years of hard work?",
+  //   option: ["A", "B", "C", "D"],
+  //   Answer: "B",
+  // },
   // {
   //   questionNumber: 7,
   //   topic: "7.What do you want to be?",
   //   description:
-  //     "She reached her goal, exhausted. Even more chilling to her was that the euphoria that she thought she'd feel upon reaching it wasn't there. Something wasn't right. Was this the only feeling she'd have for over five years of hard work?",
+  //     "She reached her goal, exhausteD: Even more chilling to her was that the euphoria that she thought she'd feel upon reaching it wasn't there. Something wasn't right. Was this the only feeling she'd have for over five years of hard work?",
   //   option: ["A", "B", "C", "D"],
   //   Answer: "B",
   // },
@@ -67,7 +67,7 @@ const Questions: QuestionsProps[] = [
   //   questionNumber: 8,
   //   topic: "8.What do you want to be?",
   //   description:
-  //     "She reached her goal, exhausted. Even more chilling to her was that the euphoria that she thought she'd feel upon reaching it wasn't there. Something wasn't right. Was this the only feeling she'd have for over five years of hard work?",
+  //     "She reached her goal, exhausteD: Even more chilling to her was that the euphoria that she thought she'd feel upon reaching it wasn't there. Something wasn't right. Was this the only feeling she'd have for over five years of hard work?",
   //   option: ["A", "B", "C", "D"],
   //   Answer: "B",
   // },
@@ -75,7 +75,7 @@ const Questions: QuestionsProps[] = [
   //   questionNumber: 9,
   //   topic: "9.What do you want to be?",
   //   description:
-  //     "She reached her goal, exhausted. Even more chilling to her was that the euphoria that she thought she'd feel upon reaching it wasn't there. Something wasn't right. Was this the only feeling she'd have for over five years of hard work?",
+  //     "She reached her goal, exhausteD: Even more chilling to her was that the euphoria that she thought she'd feel upon reaching it wasn't there. Something wasn't right. Was this the only feeling she'd have for over five years of hard work?",
   //   option: ["A", "B", "C", "D"],
   //   Answer: "B",
   // },
@@ -83,7 +83,7 @@ const Questions: QuestionsProps[] = [
   //   questionNumber: 10,
   //   topic: "10.What do you want to be?",
   //   description:
-  //     "She reached her goal, exhausted. Even more chilling to her was that the euphoria that she thought she'd feel upon reaching it wasn't there. Something wasn't right. Was this the only feeling she'd have for over five years of hard work?",
+  //     "She reached her goal, exhausteD: Even more chilling to her was that the euphoria that she thought she'd feel upon reaching it wasn't there. Something wasn't right. Was this the only feeling she'd have for over five years of hard work?",
   //   option: ["A", "B", "C", "D"],
   //   Answer: "B",
   // },
