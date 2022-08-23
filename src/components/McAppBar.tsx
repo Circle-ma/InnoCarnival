f4a6b2c3bd5721cfe5f24978546051691ee1ff26
Circle-ma <a859576084@gmail.com>
2022-08-23 15:18:26 +0800

    hide menu

--- src/components/McAppBar.tsx
+++ src/components/McAppBar.tsx
@@ -2,23 +2,23 @@ import AppBar from "@mui/material/AppBar";
 import Box from "@mui/material/Box";
 import Toolbar from "@mui/material/Toolbar";
 import Typography from "@mui/material/Typography";
-import { IconButton, Menu, MenuItem } from "@mui/material";
-import MenuIcon from "@mui/icons-material/Menu";
+// import { IconButton } from "@mui/material";
+// import MenuIcon from "@mui/icons-material/Menu";
 import React from "react";
-import { useNavigate } from "react-router-dom";
+// import { useNavigate } from "react-router-dom";
 
 const McAppBar: React.FC<{
   linkList: { navigate: string; projectName: string }[];
 }> = ({ linkList }) => {
-  const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
-  const open = Boolean(anchorEl);
-  const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
-    setAnchorEl(event.currentTarget);
-  };
-  const handleClose = () => {
-    setAnchorEl(null);
-  };
-  const navigate = useNavigate();
+  // const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
+  // const open = Boolean(anchorEl);
+  // const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+  //   setAnchorEl(event.currentTarget);
+  // };
+  // const handleClose = () => {
+  //   setAnchorEl(null);
+  // };
+  // const navigate = useNavigate();
   return (
     <Box sx={{ flexGrow: 1 }}>
       <AppBar position="static">
@@ -32,10 +32,10 @@ const McAppBar: React.FC<{
           >
             創新科技嘉年華InnoCarnival
           </Typography>
-          <IconButton color="inherit" sx={{ mr: 2 }} onClick={handleClick}>
+          {/* <IconButton color="inherit" sx={{ mr: 2 }} onClick={handleClick}>
             <MenuIcon fontSize="large" />
-          </IconButton>
-          <Menu anchorEl={anchorEl} open={open} onClose={handleClose}>
+          </IconButton> */}
+          {/* <Menu anchorEl={anchorEl} open={open} onClose={handleClose}>
             {linkList.map((project) => {
               return (
                 <MenuItem
@@ -48,7 +48,7 @@ const McAppBar: React.FC<{
                 </MenuItem>
               );
             })}
-          </Menu>
+          </Menu> */}
         </Toolbar>
       </AppBar>
     </Box>
