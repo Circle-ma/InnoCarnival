a245fabade84763575a700be1af03bb439c487d1
Circle-ma <a859576084@gmail.com>
2022-09-14 23:43:22 +0800

    learn from apple

--- src/McComponents/Tiltle.tsx
+++ src/McComponents/Tiltle.tsx
@@ -18,6 +18,12 @@ const Tiltle: React.FC<{ projectName: string }> = ({ projectName }) => {
           align="center"
           padding={5}
           color="white"
+          // color="transparent"
+          // sx={{
+          //   backgroundImage:
+          //     "linear-gradient(90deg,#2ca2b4,#5598de 24%,#7f87ff 45%,#f65aad 76%,#ec3d43);",
+          //   WebkitBackgroundClip: "text",
+          // }}
           fontWeight="bold"
         >
           {projectName}
